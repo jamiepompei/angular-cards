4c36bde39c83872370e6f9407ae5b3316d2a1944
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-02-04 09:37:28 -0500

    added data for cards, created three cards, updated titles per each card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+ posts = [
+   { title: 'Cleo',
+  imageUrl: 'assets/cleo.JPG',
+  username: 'cleo',
+  content: 'The fearless leader of this gang of sheppies.'
+  },
+  { title: 'Ziva',
+    imageUrl: 'assets/ziva.JPG',
+    username: 'ziva',
+    content: 'The purveyor of cuddles and kisses.'
+  },
+  { title: 'Jerry Lee',
+    imageUrl: 'assets/jerrylee.jpg',
+    username: 'jerry lee',
+    content: 'The provider of pure comic relief.'
+  },
+ ];
 }
