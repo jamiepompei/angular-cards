5fe12801d72af991e93ee630ac7d7ab8ed5016f0
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-02-04 17:54:56 -0500

    jerry lee image path updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
     content: 'The purveyor of cuddles and kisses.'
   },
   { title: 'Jerry Lee',
-    imageUrl: 'assets/jerrylee.JPG',
+    imageUrl: 'assets/jerrylee.jpg',
     username: 'jerry lee',
     content: 'The provider of pure comic relief.'
   },
